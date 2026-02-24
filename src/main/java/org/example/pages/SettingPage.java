333a014da5ac33acd68c8c15a4595702ab4ddf98
2464120 <Nikhil.Anand@cognizant.com>
2026-02-24 14:38:35 +0530

    correction

--- src/main/java/org/example/pages/SettingPage.java
+++ src/main/java/org/example/pages/SettingPage.java
@@ -1,14 +1,10 @@
 package org.example.pages;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
 import java.util.List;
 import java.util.stream.Collectors;
 
